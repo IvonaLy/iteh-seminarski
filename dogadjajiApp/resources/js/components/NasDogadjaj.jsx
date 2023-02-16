6801fff8c2eb6c6ce865577a30238694a41e1bbd
miticandrii <mitic.andrijana.9@gmail.com>
2023-02-16 14:11:55 +0100

    sitne ispravke

--- dogadjajiApp/resources/js/components/NasDogadjaj.jsx
+++ dogadjajiApp/resources/js/components/NasDogadjaj.jsx
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import { useState } from 'react';
+ 
  
  
  
@@ -22,9 +21,11 @@ import { useState } from 'react';
         </div>
         <div className="half">
           <div className="description">
-            <p>   {dogadjaj.description.text}</p>
+            <p>  <b> {dogadjaj.description.text}</b></p>
+            <p>   While not technically a part of Belgrade itself, Avala is a nearby mountain that overlooks the city like some sort of angry big brother, in the familial sense, as opposed to the Orwellian. It is easy to imagine the minds behind 1984’s main antagonist directing traffic from Avala Tower, however, a 205-metre tall spindle that was destroyed by NATO in 1999 before being rebuilt in 2009. This is the tallest tower in the Balkans, and it is arguably the number one attraction at Avala. The Monument to the Unknown Hero gives it a good run for its money, but the tower just about wins out.</p>
+
           </div>
-          <span className="stock"><i className="fa fa-pen"></i>JOS:  {dogadjaj.capacity} KARATA</span>
+          <span className="stock"> JOS:  {dogadjaj.capacity} KARATA</span>
           
         </div>
       </div>
